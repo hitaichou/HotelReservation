4e63902b668c871d40a1600ce2045f9d77865041
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-07-18 10:05:17 -0300

    Exemplo de reserva de hotel. Implementado classe Reservation. Neste exemplo, está sendo comparado uma forma usando if-else e try-catch.

---
+++ HotelReservation/Entities/Reservation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+        //CONSTRUTORES
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        //MÉTODOS
+        public int Duration()
+        {
+            //pegar diferença entre duas datas (diferença entre um instante e outro)
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            //para pegar a duração em dias
+            //A propriedade TotalDays é um double
+            //para converter para inteiro, é preciso usar CASTING
+            return (int)duration.TotalDays;
+        }
+
+        //Atualizo as datas das reservas
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+
+        //Usando sobrescrita para imprimir os dados da classe 
+        //no programa principal
+        public override string ToString()
+        {
+            return "Room "
+                 + RoomNumber
+                 + ", check-in: "
+                 + CheckIn.ToString("dd/MM/yyyy") //formantando o tipo data
+                 + ", check-out: "
+                 + CheckOut.ToString("dd/MM/yyyy")
+                 + ", "
+                 + Duration() //chama a função
+                 + " nights.";
+        }
+    }
+}
--- HotelReservation/Program.cs
+++ HotelReservation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HotelReservation.Entities;
 
 namespace HotelReservation
 {
@@ -6,7 +7,52 @@ namespace HotelReservation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Room NUmber: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            //Forma ruim de teste
+            //Usando if-else, os testes são feitos repetidos no código continuamente.
+            //A lógica da reseva deveria estar dentro de um bloco só.
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Reservation error: Check-out date must be after check-in date.");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation = new Reservation(number, checkIn, checkOut);
+
+                //Pego o instante atual
+                DateTime now = DateTime.Now;
+                if(checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Reservation error: Reservation dates for update must be future dates.");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Reservation error: Check-out date must be after check-in date.");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+
+            }
+           
         }
     }
 }
